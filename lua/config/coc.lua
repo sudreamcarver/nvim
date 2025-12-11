84cef4aaf69f7c71c8a30899c7bd0cb60021ee05
carver <lt72496@outlook.com>
2025-12-11 21:34:45 +0800

    .

--- lua/config/coc.lua
+++ lua/config/coc.lua
@@ -78,6 +78,3 @@ vim.api.nvim_create_autocmd("CursorHold", {
 keyset("n", "<leader>rn", "<Plug>(coc-rename)", { silent = true })
 
 
--- Formatting selected code
-keyset("x", "<leader>f", "<Plug>(coc-format-selected)", { silent = true })
-keyset("n", "<leader>f", "<Plug>(coc-format-selected)", { silent = true })
